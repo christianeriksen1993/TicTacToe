935efc42376ddf1c39d1293875a691e72257ae9d
christianeriksen1993 <christian.eriksen1993@gmail.com>
2024-07-20 20:38:10 +0200

    Add functions is_valid_move and process_move to game logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -38,34 +38,38 @@ class TicTacToeGame:
     
     def _get_winning_combos(self):
         rows = [
-                [(move.row, move.col) for move in row]
-                for row in self._current_moves
+                [(cord.row, cord.col) for cord in row] #Creates a list of coordinates for each cell in the same row
+                for row in self._current_moves #Iterates the above over the total number of rows
             ]
         columns = [list(col) for col in zip(*rows)]
         first_diagonal = [row[i] for i, row in enumerate(rows)]
         second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
-        print(rows)
         return rows + columns + [first_diagonal, second_diagonal]
-
-_current_moves = [
-            [Move(row,col) for col in range(3)]
-            for row in range(3)
-        ]
-
-rows = [
-            [(move.row, move.col) for move in row]
-                for row in _current_moves
-            ]
-columns = [list(col) for col in zip(*rows)]
-first_diagonal = [row[i] for i, row in enumerate(rows)]
-second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
-print(rows)
-#return rows + columns + [first_diagonal, second_diagonal]
-
-
-     
-
-
+    
+    def is_valid_move(self, move):
+        #Return True if move is valid, and False otherwise
+        row, col = move.row, move.col
+        move_was_not_played = self._current_moves[row][col].label == ""
+        no_winner = not self._has_winner
+        return no_winner and move_was_not_played
+    
+    def process_move(self, move):
+        #Process the current move and check if it's a win.
+        row, col = move.row, move.col
+        self._current_moves[row][col] = move
+        for combo in self._winning_combos:
+            results = set(
+                self._current_moves[n][m].label
+                for n, m in combo
+            )
+            is_win = (len(results) == 1) and ("" not in results)
+            if is_win:
+                self._has_winner = True
+                self.winner_combo = combo
+                break
+    
+    def has_winner(self):
+        return self._has_winner
 
 
 class TicTacToeBoard(tk.Tk):
